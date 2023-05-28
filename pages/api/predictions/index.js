d36e0d71df51d931a79f9e78a9975717f771ea5d
Ivan Molto <ivanmolto@gmail.com>
2023-05-28 16:51:29 +0200

    chore: Handle prediction creation requests

---
+++ pages/api/predictions/index.js
@@ -0,0 +1,25 @@
+export default async function handler(req, res) {
+  const response = await fetch("https://api.replicate.com/v1/predictions", {
+    method: "POST",
+    headers: {
+      Authorization: `Token ${process.env.NEXT_PUBLIC_REPLICATE_API_KEY}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      version:
+        "db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf",
+      input: { prompt: req.body.prompt },
+    }),
+  });
+
+  if (response.status !== 201) {
+    let error = await response.json();
+    res.statusCode = 500;
+    res.end(JSON.stringify({ detail: error.detail }));
+    return;
+  }
+
+  const prediction = await response.json();
+  res.statusCode = 201;
+  res.end(JSON.stringify(prediction));
+}
