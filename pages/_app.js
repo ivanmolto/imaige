0d0b92250cd5e10fab1fd62deaf3eed67177545d
Ivan Molto <ivanmolto@gmail.com>
2023-05-31 15:51:58 +0200

    chore: Add UserProvider

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,23 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { EtherspotTransactionKit } from "@etherspot/transaction-kit";
+import * as ethers from "ethers";
+import { UserProvider } from "@auth0/nextjs-auth0/client";
+
+import { Montserrat } from "next/font/google";
+const montserrat = Montserrat({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const randomWallet = ethers.Wallet.createRandom();
+  const providerWallet = new ethers.Wallet(randomWallet.privateKey);
+  console.log(providerWallet);
+
+  return (
+    <UserProvider>
+      <EtherspotTransactionKit provider={providerWallet} chainId={1000}>
+        <div className={montserrat.className}>
+          <Component {...pageProps} />
+        </div>
+      </EtherspotTransactionKit>
+    </UserProvider>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,118 +1,340 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { useState } from "react";
+import { useUser } from "@auth0/nextjs-auth0/client";
+import Profile from "../components/profile";
 
-const inter = Inter({ subsets: ['latin'] })
+import Head from "next/head";
+import Image from "next/image";
 
-export default function Home() {
+import { Dialog } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+
+const Home = () => {
+  const { user, error, isLoading } = useUser();
+  console.log(user);
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const [prediction, setPrediction] = useState(null);
+  const [errorAi, setErrorAi] = useState(null);
+
+  const [imageUrl, setImageUrl] = useState("");
+  const [prompt, setPrompt] = useState("");
+  const [generatingImage, setGeneratingImage] = useState(false);
+  const [minting, setMinting] = useState(false);
+  const [readyToMint, setReadyToMint] = useState(false);
+
+  const [mynfts, setMynfts] = useState([]);
+  const [myNftsLoading, setMynftsLoading] = useState(true);
+
+  const [loginLoading, setLoginLoading] = useState(false);
+  const [walletAddress, setWalletAddress] = useState();
+
+  const handlePromptChange = (e) => {
+    let promptValue = e.target.value;
+    setPrompt(promptValue);
+  };
+
+  const checkStatus = async (clear, id) => {
+    const response = await fetch("/api/predictions/" + id);
+    let prediction = await response.json();
+    if (response.status !== 200) {
+      setErrorAi(prediction.detail);
+      return;
+    }
+    setPrediction(prediction);
+    if (prediction.status == "succeeded") {
+      setImageUrl(prediction.output[prediction.output.length - 1]);
+      clearInterval(clear);
+      setPrompt("");
+      setGeneratingImage(false);
+      setReadyToMint(true);
+    }
+  };
+
+  const generateImage = async () => {
+    setReadyToMint(false);
+    setImageUrl("");
+    setGeneratingImage(true);
+    const response = await fetch("/api/predictions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt: `${prompt}`,
+        image_dimensions: "512x512",
+      }),
+    });
+    let prediction = await response.json();
+    if (response.status !== 201) {
+      setErrorAi(prediction.detail);
+      return;
+    }
+    setPrediction(prediction);
+
+    let clear = setInterval(() => {
+      checkStatus(clear, prediction.id);
+    }, 2000);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await generateImage();
+  };
+
+  const generateIpfsHash = async () => {
+    try {
+      const now = new Date();
+      let timestamp = date.format(now, "YYYY/MM/DD HH:mm:ss");
+      var data = JSON.stringify({
+        pinataContent: {
+          url: imageUrl,
+          owner: walletAddress,
+          timestamp: timestamp,
+        },
+      });
+      var config = {
+        method: "post",
+        url: "https://api.pinata.cloud/pinning/pinJSONToIPFS",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${pinata_jwt}`,
+        },
+        data: data,
+      };
+
+      const res = await axios(config);
+      console.log(res.data);
+      return res.data.IpfsHash;
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const mintNFT = async () => {
+    console.log("mintNFT");
+    setMinting(true);
+    let ipfsHash = await generateIpfsHash();
+    console.log(ipfsHash);
+
+    let iface = new ethers.utils.Interface(CONTRACT_ABI);
+    let tokenURI = `https://ipfs.io/ipfs/${ipfsHash}`;
+    let recipient = walletAddress;
+    let tx = iface.encodeFunctionData("mintNFT", [recipient, tokenURI]);
+    setMinting(false);
+  };
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <>
+      <Head>
+        <title>Imaige | Convert your AI dreams into NFTs</title>
+      </Head>
+      <div className="bg-white">
+        <header className="absolute inset-x-0 top-0 z-50">
+          <nav
+            className="flex items-center justify-between p-6 lg:px-8"
+            aria-label="Global"
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+            <div className="flex lg:flex-1">
+              <a href="#" className="-m-1.5 p-1.5">
+                <span className="sr-only">Your Company</span>
+                <Image
+                  src="/imaige.svg"
+                  alt="Imaige Logo"
+                  width={36}
+                  height={36}
+                  priority
+                />
+              </a>
+            </div>
+            <div className="flex lg:hidden">
+              <button
+                type="button"
+                className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+                onClick={() => setMobileMenuOpen(true)}
+              >
+                <span className="sr-only">Open main menu</span>
+                <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+              </button>
+            </div>
+
+            <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+              {user && (
+                <a
+                  href="/api/auth/logout"
+                  className="text-sm font-semibold leading-6 text-gray-900"
+                >
+                  Log out <span aria-hidden="true">&rarr;</span>
+                </a>
+              )}
+            </div>
+          </nav>
+          <Dialog
+            as="div"
+            className="lg:hidden"
+            open={mobileMenuOpen}
+            onClose={setMobileMenuOpen}
+          >
+            <div className="fixed inset-0 z-50" />
+            <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+              <div className="flex items-center justify-between">
+                <a href="#" className="-m-1.5 p-1.5">
+                  <span className="sr-only">Your Company</span>
+                  <img
+                    className="h-8 w-auto"
+                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                    alt=""
+                  />
+                </a>
+                <button
+                  type="button"
+                  className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                  onClick={() => setMobileMenuOpen(false)}
+                >
+                  <span className="sr-only">Close menu</span>
+                  <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                </button>
+              </div>
+              <div className="mt-6 flow-root">
+                <div className="-my-6 divide-y divide-gray-500/10">
+                  <div className="py-6">
+                    {user && (
+                      <a
+                        href="#"
+                        className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                      >
+                        Log in
+                      </a>
+                    )}
+                    {!user && (
+                      <a
+                        href="#"
+                        className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                      >
+                        Log out
+                      </a>
+                    )}
+                  </div>
+                </div>
+              </div>
+            </Dialog.Panel>
+          </Dialog>
+        </header>
+
+        <div className="relative isolate px-6 pt-14 lg:px-8">
+          <div
+            className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+            aria-hidden="true"
+          >
+            <div
+              className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+              style={{
+                clipPath:
+                  "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+              }}
             />
-          </a>
+          </div>
+          {!user && (
+            <div className="mt-32 mx-auto max-w-2xl py-8 sm:py-12 lg:py-14">
+              <div className="text-center">
+                <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl mb-4">
+                  Dream something
+                  <br /> Mint it as an NFT
+                </h1>
+                <p className="mt-6 text-lg leading-8 text-gray-600">
+                  No need to create or connect a wallet. We handle it for you
+                  with Etherspot. Login directly through your Socials with
+                  Auth0.
+                </p>
+                <div className="mt-10 flex items-center justify-center gap-x-6">
+                  <a
+                    href="/api/auth/login"
+                    className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  >
+                    Get started
+                  </a>
+                </div>
+              </div>
+            </div>
+          )}
+          {user && (
+            <div className="mt-32 mx-auto max-w-2xl py-8 sm:py-12 lg:py-14">
+              <div className="text-center">
+                <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl mb-4">
+                  Dream something and Mint it as an NFT
+                </h1>
+                <div
+                  htmlFor="prompt"
+                  className="block text-sm font-medium leading-6 text-gray-900 hover:underline"
+                >
+                  <a
+                    href="https://replicate.com/stability-ai/stable-diffusion"
+                    rel="noreferrer"
+                    target="_blank"
+                  >
+                    Prompt to Stability AI - Stable Diffusion
+                  </a>
+                </div>
+                <form className="mt-2" onSubmit={handleSubmit}>
+                  <input
+                    type="text"
+                    name="prompt"
+                    value={prompt}
+                    onChange={handlePromptChange}
+                    id="prompt"
+                    className="block w-full rounded-md border-0 p-4 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    placeholder="Enter a prompt to display an image"
+                  />
+
+                  <div className="mt-4 flex items-center justify-center gap-x-6">
+                    <button
+                      type="submit"
+                      className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    >
+                      I&#39;m Feeling Lucky
+                    </button>
+                    <button
+                      onClick={mintNFT}
+                      disabled={generatingImage && !readyToMint && !imageUrl}
+                      className="rounded-md bg-black px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-gray-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+                    >
+                      Mint NFT
+                    </button>
+                  </div>
+                </form>
+                {errorAi && <div>{errorAi}</div>}
+                {prediction && (
+                  <div className="mt-4">
+                    <p className="text-sm italic">
+                      Status: AI generated image {prediction.status}
+                    </p>
+
+                    {imageUrl && prediction.output && (
+                      <div className="w-full aspect-square relative">
+                        <Image fill src={imageUrl} alt="output" sizes="100vw" />
+                      </div>
+                    )}
+                  </div>
+                )}
+                <Profile />
+              </div>
+            </div>
+          )}
+          <div
+            className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+            aria-hidden="true"
+          >
+            <div
+              className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+              style={{
+                clipPath:
+                  "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+              }}
+            />
+          </div>
         </div>
       </div>
+    </>
+  );
+};
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default Home;
